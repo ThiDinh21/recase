6c24964bde57b5db87409c5f2a9827fdc720cffe
Thi Dinh <vietthidinh2001@gmail.com>
2021-12-29 04:22:17 +0200

    feat: add title_case and write test for it

--- src/lib.rs
+++ src/lib.rs
@@ -24,16 +24,6 @@ impl Recase {
         self.original_text.clone()
     }
 
-    pub fn sentence_case(&self) -> String {
-        let mut words = self.words.clone().into_iter();
-        match words.next() {
-            None => panic!("The field \"words\" is empty"),
-            Some(s) => {
-                utils::uppercase_first_letter(&s) + " " + &words.collect::<Vec<String>>().join(" ")
-            }
-        }
-    }
-
     pub fn normal_case(&self) -> String {
         self.words
             .clone()
@@ -51,6 +41,45 @@ impl Recase {
                     .map(|w| utils::uppercase_first_letter(&w))
                     .collect::<Vec<String>>()
                     .join("")
+            }
+        }
+    }
+
+    pub fn pascal_case(&self) -> String {
+        let mut words = self.words.clone().into_iter();
+        match words.next() {
+            None => panic!("The field \"words\" is empty"),
+            Some(s) => {
+                utils::uppercase_first_letter(&s)
+                    + &words
+                        .map(|w| utils::uppercase_first_letter(&w))
+                        .collect::<Vec<String>>()
+                        .join("")
+            }
+        }
+    }
+
+    pub fn sentence_case(&self) -> String {
+        let mut words = self.words.clone().into_iter();
+        match words.next() {
+            None => panic!("The field \"words\" is empty"),
+            Some(s) => {
+                utils::uppercase_first_letter(&s) + " " + &words.collect::<Vec<String>>().join(" ")
+            }
+        }
+    }
+
+    pub fn title_case(&self) -> String {
+        let mut words = self.words.clone().into_iter();
+        match words.next() {
+            None => panic!("The field \"words\" is empty"),
+            Some(s) => {
+                utils::uppercase_first_letter(&s)
+                    + " "
+                    + &words
+                        .map(|w| utils::uppercase_first_letter(&w))
+                        .collect::<Vec<String>>()
+                        .join(" ")
             }
         }
     }
@@ -84,24 +113,6 @@ mod recase_tests {
     }
 
     #[test]
-    fn test_sentence_case() {
-        let recase = Recase::new("who_is_god_and_why_is_she_Matsuri".to_string());
-        assert_eq!(recase.sentence_case(), "Who is god and why is she matsuri");
-
-        let recase = Recase::new("誰_is_god_and_why_is_she_Matsuri".to_string());
-        assert_eq!(recase.sentence_case(), "誰 is god and why is she matsuri");
-
-        let recase = Recase::new("WHO_is_god_and_why_is_she_Matsuri".to_string());
-        assert_eq!(
-            recase.sentence_case(),
-            "W h o is god and why is she matsuri"
-        );
-
-        let recase = Recase::new("ßho_is_god_and_why_is_she_Matsuri".to_string());
-        assert_eq!(recase.sentence_case(), "SSho is god and why is she matsuri");
-    }
-
-    #[test]
     fn test_normal_case() {
         let recase = Recase::new("who_is_god_and_why_is_she_Matsuri".to_string());
         assert_eq!(recase.normal_case(), "who is god and why is she matsuri");
@@ -129,5 +140,53 @@ mod recase_tests {
 
         let recase = Recase::new("ßho_is_god_and_why_is_ßhe?_Mätßuri".to_string());
         assert_eq!(recase.camel_case(), "ßhoIsGodAndWhyIsSShe?Mätßuri");
+    }
+
+    #[test]
+    fn test_pascal_case() {
+        let recase = Recase::new("who_is_god_and_why_is_she_Matsuri".to_string());
+        assert_eq!(recase.pascal_case(), "WhoIsGodAndWhyIsSheMatsuri");
+
+        let recase = Recase::new("誰_is_god_and_why_is_she_Matsuri".to_string());
+        assert_eq!(recase.pascal_case(), "誰IsGodAndWhyIsSheMatsuri");
+
+        let recase = Recase::new("WHO_is_god_and_Why is she_Matsuri".to_string());
+        assert_eq!(recase.pascal_case(), "WHOIsGodAndWhyIsSheMatsuri");
+
+        let recase = Recase::new("ßho_is_god_and_why_is_ßhe?_Mätßuri".to_string());
+        assert_eq!(recase.pascal_case(), "SShoIsGodAndWhyIsSShe?Mätßuri");
+    }
+
+    #[test]
+    fn test_sentence_case() {
+        let recase = Recase::new("who_is_god_and_why_is_she_Matsuri".to_string());
+        assert_eq!(recase.sentence_case(), "Who is god and why is she matsuri");
+
+        let recase = Recase::new("誰_is_god_and_why_is_she_Matsuri".to_string());
+        assert_eq!(recase.sentence_case(), "誰 is god and why is she matsuri");
+
+        let recase = Recase::new("WHO_is_god_and_why_is_she_Matsuri".to_string());
+        assert_eq!(
+            recase.sentence_case(),
+            "W h o is god and why is she matsuri"
+        );
+
+        let recase = Recase::new("ßho_is_god_and_why_is_she_Matsuri".to_string());
+        assert_eq!(recase.sentence_case(), "SSho is god and why is she matsuri");
+    }
+
+    #[test]
+    fn test_title_case() {
+        let recase = Recase::new("who_is_god_and_why_is_she_Matsuri".to_string());
+        assert_eq!(recase.title_case(), "Who Is God And Why Is She Matsuri");
+
+        let recase = Recase::new("誰_is_god_and_why_is_she_Matsuri".to_string());
+        assert_eq!(recase.title_case(), "誰 Is God And Why Is She Matsuri");
+
+        let recase = Recase::new("WHO_is_god_and_Why is she_Matsuri".to_string());
+        assert_eq!(recase.title_case(), "W H O Is God And Why Is She Matsuri");
+
+        let recase = Recase::new("ßho_is_god_and_why_is_ßhe?_Mätßuri".to_string());
+        assert_eq!(recase.title_case(), "SSho Is God And Why Is SShe? Mätßuri");
     }
 }
