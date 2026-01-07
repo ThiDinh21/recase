f1441af060fad2e02c21d1ce3c914488cc0e32d2
Thi Dinh <vietthidinh2001@gmail.com>
2026-01-07 01:35:51 +0100

    chore: update example

--- examples/main.rs
+++ examples/main.rs
@@ -1,7 +1,10 @@
-use recase::ReCase;
+use recase::{Casing, ReCase};
 
 fn main() {
     const INPUT: &str = "Löng and meaningless-Ẽxample_Text";
+
+    // Using the Casing Trait
+    println!("{}", INPUT.to_kebab_case()); // Prints "löng-and-meaningless-ẽxample-text"
 
     let recase1 = ReCase::new(INPUT);
     let recase2 = ReCase::new(String::from(INPUT));
